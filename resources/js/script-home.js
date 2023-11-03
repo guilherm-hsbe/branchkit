8387775e3d5258faff3657ca5591e3175cc3d627
guilherm-hsbe <guilherm3hsb@gmail.com>
2023-11-03 09:52:15 -0300

    feat(js): Configura scripts customizados da home em DOM
    Configura os scripts customizados da página home para DOMContentLoaded.

--- resources/js/script-home.js
+++ resources/js/script-home.js
@@ -1,82 +1,84 @@
-/* FUNÇÃO ------------------------------|
- * Atualiza o texto aleatório a cada 2 segundos.
- * Gera um texto de 4 caracteres aleatórios, incluindo letras e números,
- * e o exibe no elemento "randomText".
- */
-function generateRandomText() {
-    const characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@@##&&";
-    let randomText = "";
+document.addEventListener('DOMContentLoaded', function(){
+    /* FUNÇÃO ------------------------------|
+    * Atualiza o texto aleatório a cada 2 segundos.
+    * Gera um texto de 4 caracteres aleatórios, incluindo letras e números,
+    * e o exibe no elemento "randomText".
+    */
+    function generateRandomText() {
+        const characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@@##&&";
+        let randomText = "";
 
-    for (let i = 0; i < 6; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        randomText += characters.charAt(randomIndex);
+        for (let i = 0; i < 6; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            randomText += characters.charAt(randomIndex);
+        }
+        return randomText;
     }
-    return randomText;
-}
 
-function updateRandomText() {
-    const randomTextElement = document.getElementById("randomText");
-    randomTextElement.style.opacity = 0;
+    function updateRandomText() {
+        const randomTextElement = document.getElementById("randomText");
+        randomTextElement.style.opacity = 0;
 
-    setTimeout(function () {
-        randomTextElement.textContent = generateRandomText();
-        randomTextElement.style.opacity = 1;
-        setTimeout(updateRandomText, 2000);
-    }, 250);
-}
-updateRandomText();
-
-/* FUNÇÃO ------------------------------|
- * Atualiza o 'last-commit-date' com a data do último commit.
- * utiliza API do GitHub para obter as informações de commit do repo.
- */
-const apiUrl = 'https://api.github.com/repos/guilherm-hsbe/BranchKit/commits/main';
-async function fetchLastCommitDate() {
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        const commitDate = new Date(data.commit.committer.date);
-        const formattedDate = commitDate.toLocaleDateString();
-        document.getElementById('last-commit-date').textContent = formattedDate;
-    } catch (error) {
-        console.error('(erro ao buscar o último commit:', error, ')');
-        document.getElementById('last-commit-date').textContent = '(erro ao buscar a data do último commit.)';
+        setTimeout(function () {
+            randomTextElement.textContent = generateRandomText();
+            randomTextElement.style.opacity = 1;
+            setTimeout(updateRandomText, 2000);
+        }, 250);
     }
-}
-fetchLastCommitDate();
+    updateRandomText();
 
-/* FUNÇÃO ------------------------------|
- * Copia o comando de cópia do repo e muda
- * o ícone do botão por três segundos.
- */
-function copyGitClone() {
-    const texto = document.getElementById("clone-repo-text");
-    const textoParaCopiar = texto.innerText;
+    /* FUNÇÃO ------------------------------|
+    * Atualiza o 'last-commit-date' com a data do último commit.
+    * utiliza API do GitHub para obter as informações de commit do repo.
+    */
+    const apiUrl = 'https://api.github.com/repos/guilherm-hsbe/BranchKit/commits/main';
+    async function fetchLastCommitDate() {
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            const commitDate = new Date(data.commit.committer.date);
+            const formattedDate = commitDate.toLocaleDateString();
+            document.getElementById('last-commit-date').textContent = formattedDate;
+        } catch (error) {
+            console.error('(erro ao buscar o último commit:', error, ')');
+            document.getElementById('last-commit-date').textContent = '(erro ao buscar a data do último commit.)';
+        }
+    }
+    fetchLastCommitDate();
 
-    const textarea = document.createElement("textarea");
-    textarea.value = textoParaCopiar;
+    /* FUNÇÃO ------------------------------|
+    * Copia o comando de cópia do repo e muda
+    * o ícone do botão por três segundos.
+    */
+    function copyGitClone() {
+        const texto = document.getElementById("clone-repo-text");
+        const textoParaCopiar = texto.innerText;
 
-    document.body.appendChild(textarea);
+        const textarea = document.createElement("textarea");
+        textarea.value = textoParaCopiar;
 
-    textarea.select();
-    document.execCommand("copy");
+        document.body.appendChild(textarea);
 
-    document.body.removeChild(textarea);
-}
+        textarea.select();
+        document.execCommand("copy");
 
-function changeIcon() {
-    const icone = document.getElementById("clone-repo-icon");
-    icone.classList.remove("fa-regular", "fa-copy");
-    icone.classList.add("fa-solid", "fa-check");
+        document.body.removeChild(textarea);
+    }
 
-    setTimeout(function() {
-        icone.classList.remove("fa-solid", "fa-check");
-        icone.classList.add("fa-regular", "fa-copy");
-    }, 3000);
-}
+    function changeIcon() {
+        const icone = document.getElementById("clone-repo-icon");
+        icone.classList.remove("fa-regular", "fa-copy");
+        icone.classList.add("fa-solid", "fa-check");
 
-const botao = document.getElementById("clone-repo-btn");
-botao.addEventListener("click", function() {
-    copyGitClone();
-    changeIcon();
+        setTimeout(function() {
+            icone.classList.remove("fa-solid", "fa-check");
+            icone.classList.add("fa-regular", "fa-copy");
+        }, 3000);
+    }
+
+    const botao = document.getElementById("clone-repo-btn");
+    botao.addEventListener("click", function() {
+        copyGitClone();
+        changeIcon();
+    });
 });
